b8eba03edc2eca43b813ad5bb330c6a288352f61
ThDelgado <100494591+ThDelgado@users.noreply.github.com>
2024-07-24 23:08:39 -0400

    creacion de archivo

---
+++ assets/js/my.js
@@ -0,0 +1,8 @@
+let numero1 = parseInt(prompt('ingrese primer numero:'));
+let numero2 = parseInt(prompt('ingrese segundo número:'));
+if(numero1 > numero2){
+    console.log(numero1 +'es mayor que'+ numero2)
+}
+else{
+    console.log(numero2 +' es mayor que '+ numero1 )
+}
